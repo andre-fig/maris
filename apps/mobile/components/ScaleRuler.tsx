e66fed7acd61c16f0aa16dab38fce494c600a09a
André Figueiredo <andrefigueiredo@id.uff.br>
2026-09-17 08:48:10 -0300

    style(mobile): further reduce weather blur

--- apps/mobile/components/ScaleRuler.tsx
+++ apps/mobile/components/ScaleRuler.tsx
@@ -182,7 +182,7 @@ export function ScaleRuler({
         ]}
       >
         <BlurView
-          intensity={22}
+          intensity={12}
           tint="systemMaterialDark"
           style={StyleSheet.absoluteFill}
         />
