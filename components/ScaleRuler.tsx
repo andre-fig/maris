f25f874129d6a3d6cedec7267e7eb61b7d0c5f24
André Figueiredo <andrefigueiredo@id.uff.br>
2026-09-17 01:10:05 -0300

    style: strengthen scale label outlines

--- components/ScaleRuler.tsx
+++ components/ScaleRuler.tsx
@@ -147,7 +147,7 @@ const styles = StyleSheet.create({
     color: '#000000',
     textShadowColor: '#000000',
     textShadowOffset: { width: 0, height: 0 },
-    textShadowRadius: 1.5,
+    textShadowRadius: 2,
   },
   bar: {
     height: 8,
